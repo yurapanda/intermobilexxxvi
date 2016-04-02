9c92b0cf1727c429d7ee10c2ca1c413d92917976
yurapanda <jerrome.pelacio@ama.edu.ph>
2016-04-02 09:10:44 +0800

    w~

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,7 @@ module Intermobile
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-     config.assets.initialize_on_precompile = false
-    config.serve_static_files = true 
+    config.assets.compile = true
     config.assets.enabled = true
    
   end
